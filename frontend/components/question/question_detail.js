269f9cdf0b33715c6828cd63368b2bfe7c04761b
yiyi1026 <vivian.li.uibe@gmail.com>
2017-07-24 20:52:25 -0700

    refactor question detail

--- frontend/components/question/question_detail.js
+++ frontend/components/question/question_detail.js
@@ -9,8 +9,8 @@ class QuestionDetail extends React.Component {
     this.state = {
       title: '',
       body: '',
-      id: '',
-      answers: []
+      id: ''
+
     };
   }
 
@@ -22,20 +22,28 @@ class QuestionDetail extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    console.log(this.props);
+    console.log(nextProps);
     let id = parseInt(this.props.match.params.questionId);
     let nextid = parseInt(nextProps.match.params.questionId);
     if (id !== nextid) {
       this.props.requestSingleQuestion(nextid);
+    }else{
+      let question = nextProps.questions.byId;
+      // let title = question.title
+      let {title, body, id, author, answers} = question;
+      console.log(answers);
+      this.setState({title, body, id, author});
     }
   }
 
   render() {
-    // console.log(this.state);
-    // console.log(this.props);
+    console.log(this.state);
+    console.log(this.props);
     const {answers} = this.props;
     const question = this.props.questions.byId;
-    if (question && question.author){
-      let {avatar, description, username} = question.author;
+    // if (question && question.author){
+    //   let {avatar, description, username} = question.author;
 
 
     // const {avatar} = author
@@ -44,92 +52,92 @@ class QuestionDetail extends React.Component {
     // const
     // const question = this.state;
     // console.log(this.state);
-    // return (<div>here</div>);
-    return (
-      <li>
-        <div className='container well'>
-          <div className="row">
-            <div className="reason_main">
-              Answer<span className="bullet">
-                ·
-              </span>
-              <a className="grey" href="#">
-                <span >Interpersonal Interaction</span>
-              </a>
-              <span className="bullet">
-                ·
-              </span>Topic you might like
-            </div>
-          </div>
-          <div className="row all-margin-10">
-            <div className="">
-              <a className="black bold" href={'#/questions/' + question.id} target="">
-                <span className="">{question.title}</span>
-              </a>
-            </div>
-          </div>
-          <div className="row">
-            <div className="">
-              <a href="#"><img className="img-circle pull-left" src={avatar} width="50" height="50"/></a>
-            </div>
-            <div className="left-margin-60">
-              <div className="">
-                <span>
-                  <a className="user black" href="#">{author.username}</a>
-                </span>
-                <span >,
-                </span>
-                <span>{author.description}</span>
-              </div>
-              <div className="">
-                <span>
-                  <a className="grey" href="#" target="">Answered 8h ago</a>
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="all-margin-10">
-              <span className="rendered_qtext">
-                <p>{question.body}</p>
-              </span>
-              {/* <span id=""><a className="" href="#" target="_blank">(more)</a></span> */}
-            </div>
-          </div>
-
-          <div className="row">
-            <div className=" accordion-heading">
-              <span className="left-margin-10  ">
-                <a className="Answer accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href={"#collapse" + question.id} role="button">
-                  <span className="glyphicon glyphicon-pencil"></span>Answer
-                </a>
-              </span>
-              <span className="left-margin-10">
-                <a className="Upvote" href="#">
-                  <span>Upvote</span>
-                  <span className="divider-vertical-15px"></span>
-                  <span className=" ">95</span>
-                </a>
-              </span>
-              <span className="left-margin-10">
-                <a className="Downvote grey" href="#">
-                  <span className=" ">Downvote</span>
-                </a>
-              </span>
-            </div>
-            <div id={"collapse" + question.id} className="accordion-body collapse">
-              <AnswerFormContainer question={question}/>
-
-            </div>
-          </div>
-
-        </div>
-      </li>
-
-    );
-
-    }
+    return (<div>{this.state.title}<br/><div><br/><br/><br/><br/><br/>{this.state.body}</div></div>);
+    // return (
+    //   <li>
+    //     <div className='container well'>
+    //       <div className="row">
+    //         <div className="reason_main">
+    //           Answer<span className="bullet">
+    //             ·
+    //           </span>
+    //           <a className="grey" href="#">
+    //             <span >Interpersonal Interaction</span>
+    //           </a>
+    //           <span className="bullet">
+    //             ·
+    //           </span>Topic you might like
+    //         </div>
+    //       </div>
+    //       <div className="row all-margin-10">
+    //         <div className="">
+    //           <a className="black bold" href={'#/questions/' + question.id} target="">
+    //             <span className="">{question.title}</span>
+    //           </a>
+    //         </div>
+    //       </div>
+    //       <div className="row">
+    //         <div className="">
+    //           <a href="#"><img className="img-circle pull-left" src={avatar} width="50" height="50"/></a>
+    //         </div>
+    //         <div className="left-margin-60">
+    //           <div className="">
+    //             <span>
+    //               <a className="user black" href="#">{author.username}</a>
+    //             </span>
+    //             <span >,
+    //             </span>
+    //             <span>{author.description}</span>
+    //           </div>
+    //           <div className="">
+    //             <span>
+    //               <a className="grey" href="#" target="">Answered 8h ago</a>
+    //             </span>
+    //           </div>
+    //         </div>
+    //       </div>
+    //
+    //       <div className="row">
+    //         <div className="all-margin-10">
+    //           <span className="rendered_qtext">
+    //             <p>{question.body}</p>
+    //           </span>
+    //           {/* <span id=""><a className="" href="#" target="_blank">(more)</a></span> */}
+    //         </div>
+    //       </div>
+    //
+    //       <div className="row">
+    //         <div className=" accordion-heading">
+    //           <span className="left-margin-10  ">
+    //             <a className="Answer accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href={"#collapse" + question.id} role="button">
+    //               <span className="glyphicon glyphicon-pencil"></span>Answer
+    //             </a>
+    //           </span>
+    //           <span className="left-margin-10">
+    //             <a className="Upvote" href="#">
+    //               <span>Upvote</span>
+    //               <span className="divider-vertical-15px"></span>
+    //               <span className=" ">95</span>
+    //             </a>
+    //           </span>
+    //           <span className="left-margin-10">
+    //             <a className="Downvote grey" href="#">
+    //               <span className=" ">Downvote</span>
+    //             </a>
+    //           </span>
+    //         </div>
+    //         <div id={"collapse" + question.id} className="accordion-body collapse">
+    //           <AnswerFormContainer question={question}/>
+    //
+    //         </div>
+    //       </div>
+    //
+    //     </div>
+    //   </li>
+    //
+    // );
+    //
+    // }
   }
 }
 
