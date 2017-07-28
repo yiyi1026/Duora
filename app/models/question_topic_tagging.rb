5db60f47b23086baa57a022ca96ead960e580794
yiyi1026 <vivian.li.uibe@gmail.com>
2017-07-28 09:08:58 -0700

    annotate

--- app/models/question_topic_tagging.rb
+++ app/models/question_topic_tagging.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: question_topic_taggings
+#
+#  id          :integer          not null, primary key
+#  topic_id    :integer          not null
+#  question_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class QuestionTopicTagging < ApplicationRecord
     #this table is for question to associate with topics
   validates_uniqueness_of :question_id, scope: :topic_id
--- test/models/question_topic_tagging_test.rb
+++ test/models/question_topic_tagging_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: question_topic_taggings
+#
+#  id          :integer          not null, primary key
+#  topic_id    :integer          not null
+#  question_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class QuestionTopicTaggingTest < ActiveSupport::TestCase
